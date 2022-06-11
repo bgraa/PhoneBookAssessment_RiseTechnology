b0d73818603d6c1514f85e59d137750c6a770c83
Buğra A <atasevenbugra@gmail.com>
2022-06-12 01:56:44 +0300

    added migration to ContactAPI

---
+++ PhoneBookAssessment.ContactAPI/Data/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PhoneBookAssessment.ContactAPI.Data.Entities
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+        }
+    }
+}
---
+++ PhoneBookAssessment.ContactAPI/Data/Entities/PeopleContactInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PhoneBookAssessment.ContactAPI.Data.Entities
+{
+    public class People
+    {
+        public int UUID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Firm { get; set; }
+        public PeopleContactInformation ContactInformation { get; set; }
+
+    }
+}
